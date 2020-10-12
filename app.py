21a1c31a32f4699d1ee3c3bb0483a85933fb637f
blazedsouza <blazedsouza24@yahoo.com>
2020-10-12 16:35:51 +0000

    initial_commit

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
@@ -32,15 +34,35 @@ users = [
 
 @app.route('/user', methods=['GET'])
 def get_all_users():
-    return jsonify({'users': users})
+    users = User.query.all()
+    output = []
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        output.append(user_data)
+    return jsonify({'users': output})
 
-@app.route('/user/<user_id>', methods=['GET'])
-def get_one_user():
-    return ''
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message':'No user found!'})
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+    
+
+    return jsonify({'user': user_data})
 
 @app.route('/user', methods=['POST'])
 def create_user():
-    data = request.get_json()
+    data = request.get_json(force=True)
 
     hashed_password = generate_password_hash(data['password'], method='sha256')
     new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
@@ -48,13 +70,28 @@ def create_user():
     db.session.commit()
     return jsonify({'message' : 'New user created!'})
 
-@app.route('/user/<user_id>', methods=['PUT'])
-def promote_user():
-    return ''
+@app.route('/user/<public_id>', methods=['PUT'])
+def promote_user(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
 
-@app.route('/user/<user_id>', methods=['DELETE'])
-def delete_user():
-    return ''
+    if not user:
+        return jsonify({'message':'No user found'})
+
+    user.admin=True
+    db.session.commit()
+    return jsonify({'message': 'The user has been promoted!'})
+
+@app.route('/user/<public_id>', methods=['DELETE'])
+def delete_user(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message':'No user found'})
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({'message': 'The user has been deleted!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
